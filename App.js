2397a271a1eaacf1678d6a43078d03a9f42c97b0
Adarsh Gupta <131667247+adarsh8097@users.noreply.github.com>
2023-07-09 22:52:33 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+import '../styles/App.css';
+
+
+class App extends Component{
+    render(){
+          <div id="main">
+            <ol key={'relativeList'}>
+                <li key={'relativeList1'}> Adarsh</li>
+                <li key={'relativeList2'}>Aswath</li>
+                <li key={'relativeList3'}>Kunal</li>
+                <li key={'relativeList4'}>Rupal</li>
+            </ol>
+          </div>
+
+
+    }
+}
+
+export default App;
